c4530d8861f00dd2a4ceaf007f4e94a35857aba8
Fabgwada <fabien.jeantheodore@outlook.fr>
2021-04-27 10:15:49 +0200

    Initialize DP project

--- DP/Program.cs
+++ DP/Program.cs
@@ -6,7 +6,7 @@ namespace DP
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Test Git!");
         }
     }
 }
