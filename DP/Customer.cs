61ad1a83e75b55fe25002a118df64fdf6599f934
Fabgwada <fabien.jeantheodore@outlook.fr>
2021-04-27 14:51:04 +0200

    RIP design pattern (replace if with polymorphism)

---
+++ DP/Customer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DP
+{
+    public abstract class Customer
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+    }
+
+
+    public class Lead : Customer
+    {
+
+    }
+
+    public class Buyer : Customer
+    {
+        public Invoice Invoice { get; set; }
+    }
+}
--- DP/Program.cs
+++ DP/Program.cs
@@ -1,12 +1,40 @@
-﻿using System;
+﻿#define RIP
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace DP
 {
     class Program
     {
+#if RIP
+        static Dictionary<int, Customer> Customers;
         static void Main(string[] args)
         {
-            Console.WriteLine("Test Git!");
+            Customers = new Dictionary<int, Customer>();
+            Customers.Add(1, new Lead());
+            Customers.Add(2, new Buyer());
+
+            int custIndex = 0;
+
+            while (true)
+            {
+                Console.WriteLine("Build a Lead (1), or build a Buyer (2), type 1 or 2");
+                var entry = Console.ReadLine();
+                Debug.WriteLine($"le type choisi est {entry}");
+
+                if (entry == "1" || entry == "2")
+                {
+                    custIndex = int.Parse(entry);
+                    break;
+                }
+            }
+
+            var customer = Customers.GetValueOrDefault(custIndex);
+
+            Console.WriteLine(customer.GetType());
+            Console.Read();
         }
     }
+#endif
 }
